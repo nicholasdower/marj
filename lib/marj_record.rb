0dccaf1b58dcc3cf3cad6605f850180c73e5eaac
marj-ci <nicholasdower@gmail.com>
2024-01-23 12:55:52 +0000

    Bump version to 3.0.0.pre

--- lib/marj_record.rb
+++ lib/marj_record.rb
@@ -9,7 +9,7 @@ require_relative 'marj_config'
 # See https://github.com/nicholasdower/marj
 class Marj < ActiveRecord::Base
   # The Marj version.
-  VERSION = '2.0.1'
+  VERSION = '3.0.0.pre'
 
   # Executes the job associated with this record and returns the result.
   def execute
